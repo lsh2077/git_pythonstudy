bf4f588e7054865455e0bce8051fd2533608e21d
hun-li <lsh2077@naver.com>
2019-11-19 10:15:38 +0900

    add 함수추가

--- chapter_git/hello_python.py
+++ chapter_git/hello_python.py
@@ -1,3 +1,10 @@
+def add(a,b):
+    return a+b
+
 if __name__=="__main__":
     print("hello python")
     print("hi pycharm")
+    x=10
+    y=5
+    res=add(x,y)
+    print("add({},{}) result =>{}".format(x,y,res))
