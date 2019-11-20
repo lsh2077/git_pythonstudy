0caa300504174fa3c27379e99d73f67d2d2507b3
hun-soso <lsh2077@naver.com>
2019-11-20 17:17:42 +0900

    tuple 56~57/11.20 로또번호생성

--- chapter_04/set_study.py
+++ chapter_04/set_study.py
@@ -3,7 +3,14 @@
 import random as rnd
 from chapter_04.bubblesort import bubble_sort
 
-
+def lotto_bubble_sort(random_list):
+    for i in range(len(random_list)-2):
+        for j in range(len(random_list)-2-i):
+           if random_list[j] > random_list[j+1]:
+                temp=random_list[j+1]
+                random_list[j+1]=random_list[j]
+                random_list[j]=temp
+    return random_list
 
 def lotto_generator():
     lotto_num = set()
@@ -11,9 +18,38 @@ def lotto_generator():
         lotto_num.add(rnd.randint(1,46))
     return lotto_num
 
+def lotto_winning():
+    #rnd.seed(1)
+    lotto_winning_num = set()
+    while len(lotto_winning_num) < 7:
+        lotto_winning_num.add(rnd.randint(1, 46))
+    lotto_winning_num=lotto_bubble_sort(list(lotto_winning_num))
+    return lotto_winning_num
+
+def lotto_grade(list1,list2):
+    count1=0
+    count2=0
+    for i in range(0,6):
+        count1 += list1.count(list2[i])
+    count2 +=list1.count(list2[6])
+    if count1==6: print("1등")
+    elif count1==5 and count2==1: print("2등")
+    elif count1==5:print("3등")
+    elif count1==5: print("4등")
+    elif count1==4: print("5등")
+    else: print("꽝")
 
 
 if __name__=="__main__":
-    rnd.seed(1)
-    set_lotto= list(lotto_generator())
-    print("로또 번호: {}".format(bubble_sort(set_lotto))) #정렬된 결과
+    #rnd.seed(1)
+    set_lotto=[]
+    for i in range(0,5):
+        set_lotto.append(list(lotto_generator()))
+        print("로또번호{}".format(bubble_sort(set_lotto[i]),end='\t'))
+    winning_lotto=lotto_winning()
+    print("당첨자 번호{}".format([winning_lotto[i] for i in range(0,6)]),end='   ')
+    print("보너스 번호:{}".format(winning_lotto[6]))
+    for i in range(0,5):
+        print("로또번호{}".format(bubble_sort(set_lotto[i]),end='\t'))
+        lotto_grade(set_lotto[i],winning_lotto)
+
