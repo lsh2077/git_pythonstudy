fde4f1c6ece8cc2a52fb61d5e3e7b4a38039e6be
hun-soso <lsh2077@naver.com>
2019-11-21 21:41:51 +0900

    19.11.21 11.21ex[리스트]  수정 11.21ex[dictionray] 작성

--- student_management/11.21dictionary.py
+++ student_management/11.21dictionary.py
@@ -12,16 +12,16 @@ student_management 패키지 추가
 #split() --()공간으로 쪼개
 
 """
-save_list=[]
+save_dict={}
 def first_act():
-    global save_list
+    global save_dict
     try:
         f=open("student_list.txt","r")
-        all_list=[]
         line = f.readline()
         while line:
             line_list = line.split()
-            save_list.append(line_list)
+            upload_dict={line_list[0]:{"학생이름":line_list[1],"국어점수":line_list[2],"수학점수":line_list[3],"영어점수":line_list[4]}}
+            save_dict.update(upload_dict)
             line = f.readline()
     except FileNotFoundError as e:
         print("해당파일이 존재하지않음", e,sep='\n')
@@ -37,43 +37,57 @@ def switch():
             continue
         elif a == "2":
             student_add()
+            student_edit_list1()
             continue
         elif a == "3":
             student_edit()
             continue
         elif a == "4":
             student_delete()
-            student_list()
+            student_edit_list1()
             continue
         elif a == "5":
             exit_save()
             return
         else: continue
-###
+
 def student_list():
-    global save_list
-    for i in range(len(save_list)):
-        sumscore=int(save_list[i][2])+int(save_list[i][3])+int(save_list[i][4])
-        averagescore=sumscore/3
-        print("%-4s%-4s%     s/%s/%s--%d--%0.1f"%(save_list[i][0],save_list[i][1],save_list[i][2],save_list[i][3],save_list[i][4],sumscore, averagescore))
+    global save_dict
+    for i in save_dict.keys():
+        print("{}".format(i),end=' ')
+        sumscore=int(save_dict[i]["국어점수"])+int(save_dict[i]["수학점수"])+int(save_dict[i]["영어점수"])
+        averagescore = sumscore / 3
+        for a, b in save_dict[i].items():
+            print("{} : {}".format(a, b),end=' ')
+        print("총점: {} 평균: {:.1f}".format(sumscore,averagescore))
 
-def student_edit_list(i):
-    global save_list
-    print("%-4s%-4s%     s/%s/%s"% (save_list[i][0], save_list[i][1], save_list[i][2], save_list[i][3],save_list[i][4]))
 
+def student_edit_list1():
+    global save_dict
+    for i in save_dict.keys():
+        print("{}".format(i), end=' ')
+        for a, b in save_dict[i].items():
+            print("{} : {}".format(a, b), end=' ')
+        print()
+def student_edit_list2(i):
+    print("선택된학생:\n{}".format(i), end=' ')
+    for a, b in save_dict[i].items():
+        print("{} : {}".format(a, b), end=' ')
+    print()
 
 def student_add():
-    global save_list
+    global save_dict
 
     while 1:
         sign = 0
         no=input("입력할 학생번호?(종료:0):")
-        for i in range(len(save_list)) :
-            if save_list[i][0] == no :
+        for i in save_dict.keys():
+            if no==i :
                 print("중복하는사람이있습니다 다시 입력해주세요")
-                sign=1
+                sign = 1
         if sign == 0: break
         if no == '0': return
+
     name=input("입력할 학생이름?(종료:0):")
     if name == '0': return
     kor=input("입력할 국어점수?(종료:0):")
@@ -82,43 +96,47 @@ def student_add():
     if math == '0': return
     eng=input("입력할 영어점수?(종료:0):")
     if eng == '0': return
-    save_list.append([no,name,kor,math,eng])
+    upload_dict = {no: {"학생이름": name, "국어점수": kor, "수학점수": math, "영어점수": eng}}
+    save_dict.update(upload_dict)
+
 
 def student_edit():
-    global save_list
-    student_list()
+    global save_dict
+    student_edit_list1()
+
     while 1:
         sign = 0
         no=input("수정할 학생번호?(종료:0):")
-        for i in range(len(save_list)) :
-            if save_list[i][0] == no :
+        for i in save_dict.keys():
+            if no==i :
                 area=i
                 sign = 1
         if sign == 1: break
         print("존재하지않는 번호입니다.")
         if no=='0' : return
+
     while 1:
-        student_edit_list(area)
+        student_edit_list2(area)
         a=input("--------edit--------\n 1.이름\n 2.국어점수\n 3.수학점수\n 4.영어점수\n 5.다른학생선택\n 6.종료\n*수행작업을 선택해주세요:")
         if a == "1":
             name = input("수정할 학생이름?(종료:0):")
             if name == '0': return
-            save_list[area][1]=name
+            save_dict[area]["학생이름"]=name
             continue
         elif a == "2":
             kor = input("입력할 국어점수?(종료:0):")
             if kor == '0': return
-            save_list[area][2] = kor
+            save_dict[area]["국어점수"] = kor
             continue
         elif a == "3":
             math = input("입력할 수학점수?(종료:0):")
             if math == '0': return
-            save_list[area][3] = math
+            save_dict[area]["수학점수"] = math
             continue
         elif a == "4":
             eng = input("입력할 영어점수?(종료:0):")
             if eng == '0': return
-            save_list[area][4] = eng
+            save_dict[area]["영어점"] = eng
             continue
         elif a == "5":
             student_edit()
@@ -131,26 +149,28 @@ def student_edit():
 
 
 def student_delete():
-    global save_list
-    student_list()
+    global save_dict
+    student_edit_list1()
     while 1:
         sign = 0
-        no = input("삭제할 학생번호?(종료:0):")
-        for i in range(len(save_list)):
-            if save_list[i][0] == no:
+        no = input("수정할 학생번호?(종료:0):")
+        for i in save_dict.keys():
+            if no == i:
                 area = i
                 sign = 1
         if sign == 1: break
         print("존재하지않는 번호입니다.")
         if no == '0': return
-    save_list.remove(save_list[area])
+    del save_dict[area]
 
 def exit_save():
-    global save_list
+    global save_dict
     try:
         f = open("student_list.txt", "w")
-        for i in range(len(save_list)):
-            f.write(save_list[i][0]+' '+save_list[i][1]+' '+save_list[i][2]+' '+save_list[i][3]+' '+save_list[i][4])
+        for i in save_dict.keys():
+            f.write(i+' ')
+            for a,b in save_dict[i].items():
+                f.write(' '+save_dict[i][a])
 
     except FileNotFoundError as e:
         print("해당파일이 존재하지않음", e, sep='\n')
@@ -160,6 +180,7 @@ def exit_save():
 
 
 if __name__=="__main__":
+
     first_act()
     switch()
 
